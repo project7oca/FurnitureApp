3c9ac8d082c22673a5a992445908c3e25dd169a1
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-01-30 00:23:19 +0200

    complete the contactus page

---
+++ contact.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="ie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<!--<![endif]-->
+<html lang="en">
+
+
+<!-- contact11:09-->
+
+<head>
+    <!-- Basic Page Needs -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Furnitica - Minimalist Furniture HTML Template</title>
+
+    <meta name="keywords" content="Furniture, Decor, Interior">
+    <meta name="description" content="Furnitica - Minimalist Furniture HTML Template">
+    <meta name="author" content="tivatheme">
+
+
+    <!-- Mobile Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700" rel="stylesheet">
+
+    <!-- Vendor CSS -->
+    <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="libs/nivo-slider/css/nivo-slider.css">
+    <link rel="stylesheet" href="libs/nivo-slider/css/animate.css">
+    <link rel="stylesheet" href="libs/nivo-slider/css/style.css">
+    <link rel="stylesheet" href="libs/font-material/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="libs/slider-range/css/jslider.css">
+    <link rel="stylesheet" href="libs/owl-carousel/assets/owl.carousel.min.css">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/reponsive.css">
+</head>
+
+<body id="contact" class="blog">
+    <?php include_once("./navbar.php"); ?>
+
+
+    <!-- main content -->
+    <div class="main-content">
+        <div id="wrapper-site">
+            <div id="content-wrapper">
+
+                <!-- breadcrumb -->
+                <nav class="breadcrumb-bg">
+                    <div class="container no-index">
+                        <div class="breadcrumb">
+                            <ol>
+                                <li>
+                                    <a href="#">
+                                        <span>Home</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span>Contact</span>
+                                    </a>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </nav>
+                <div id="main">
+                    <div class="page-home">
+                        <div class="container">
+                            <h1 class="text-center title-page">Contact Us</h1>
+                            <div class="row-inhert">
+                                <div class="header-contact">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-4 col-md-4">
+                                            <div class="item d-flex">
+                                                <div class="item-left">
+                                                    <div class="icon">
+                                                        <i class="zmdi zmdi-email"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="item-right d-flex">
+                                                    <div class="title">Email:</div>
+                                                    <div class="contact-content">
+                                                        <a href="mailto:support@furnitica.com">support@furnitica.com</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-4 col-md-4">
+                                            <div class="item d-flex">
+                                                <div class="item-left">
+                                                    <div class="icon">
+                                                        <i class="zmdi zmdi-home"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="item-right d-flex">
+                                                    <div class="title">Address:</div>
+                                                    <div class="contact-content">
+                                                        Amman, Jordan
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-4 col-md-4">
+                                            <div class="item d-flex justify-content-end  last">
+                                                <div class="item-left">
+                                                    <div class="icon">
+                                                        <i class="zmdi zmdi-phone"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="item-right d-flex">
+                                                    <div class="title">Hotline:</div>
+                                                    <div class="contact-content">
+                                                        0123-456-78910
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="contact-map">
+                                    <div id="map">
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1372.6181551335246!2d35.90913889780644!3d31.969947086884115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ca1dd7bca79dd%3A0x9b0416f056ff0786!2sOrange%20Digital%20Village!5e0!3m2!1sen!2sjo!4v1643494422083!5m2!1sen!2sjo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                                    </div>
+                                </div>
+                                <div class="input-contact">
+                                    <p class="text-intro text-center">“Proin gravida nibh vel velit auctor aliquet. Aenean sollicudin, lorem quis bibendum
+                                        auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit
+                                        amet nibh vultate cursus a sit amet mauris. Proin gravida nibh vel velit auctor aliquet.”
+                                    </p>
+
+                                    <!-- <p class="icon text-center">
+                                        <a href="#">
+                                            <img src="img/other/contact_mess.png" alt="img">
+                                        </a>
+                                    </p>
+
+                                    <div class="d-flex justify-content-center">
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <div class="contact-form">
+                                                <form action="#" method="post" enctype="multipart/form-data">
+                                                    <div class="form-fields">
+                                                        <div class="form-group row">
+                                                            <div class="col-md-6">
+                                                                <input class="form-control" name="name" placeholder="Your name">
+                                                            </div>
+                                                            <div class="col-md-6 margin-bottom-mobie">
+                                                                <input class="form-control" name="from" type="email" value="" placeholder="Your email">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <div class="col-md-12 margin-bottom-mobie">
+                                                                <input class="form-control" name="from" type="email" value="" placeholder="Subject">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <div class="col-md-12">
+                                                                <textarea class="form-control" name="message" placeholder="Message" rows="8"></textarea>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <button class="btn" type="submit" name="submitMessage">
+                                                            <img class="img-fl" src="img/other/contact_email.png" alt="img">Send message
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <?php include_once("./footer.php"); ?>
+
+
+    <!-- back top top -->
+    <?php include_once("./backToTopBtn.php"); ?>
+
+
+    <!-- menu mobie left -->
+
+
+    <!-- menu mobie right -->
+    <?php include_once("./menuMobileRight.php") ?>
+
+
+    <!-- Page Loader -->
+    <?php
+    // include_once("./pagePreLoader.php")
+    ?>
+
+    <!-- Vendor JS -->
+    <script src="libs/jquery/jquery.min.js"></script>
+    <script src="libs/popper/popper.min.js"></script>
+    <script src="libs/bootstrap/js/bootstrap.min.js"></script>
+    <script src="libs/nivo-slider/js/jquery.nivo.slider.js"></script>
+    <script src="libs/owl-carousel/owl.carousel.min.js"></script>
+    <script src="libs/slider-range/js/tmpl.js"></script>
+    <script src="libs/slider-range/js/jquery.dependClass-0.1.js"></script>
+    <script src="libs/slider-range/js/draggable-0.1.js"></script>
+    <script src="libs/slider-range/js/jquery.slider.js"></script>
+
+    <!-- Template JS -->
+    <script src="js/theme.js"></script>
+</body>
+
+
+<!-- contact11:10-->
+
+</html>
