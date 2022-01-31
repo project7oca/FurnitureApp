03baa0d1e26bb46552b3a5d3ca909731ad4a8e53
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-01-31 03:05:36 +0200

    handle best seller setion product detail page

--- product-detail.php
+++ product-detail.php
@@ -183,99 +183,37 @@ if (isset($_POST['newUserReview'])) {
            </div>
            <div class="product-content tab-content">
             <div class="row">
-             <div class="item col-md-12">
-              <div class="product-miniature item-one first-item d-flex">
-               <div class="thumbnail-container border">
-                <a href="product-detail.html">
-                 <img class="img-fluid image-cover" src="img/product/1.jpg" alt="img">
-                 <img class="img-fluid image-secondary" src="img/product/22.jpg" alt="img">
-                </a>
-               </div>
-               <div class="product-description">
-                <div class="product-groups">
-                 <div class="product-title">
-                  <a href="product-detail.html">Nulla et justo augue</a>
-                 </div>
-                 <div class="rating">
-                  <div class="star-content">
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
+             <?php $discount = $conn->prepare("SELECT * FROM products WHERE discount!=0 LIMIT 3");
+             $discount->execute();
+             foreach ($discount as $element) { ?>
+              <div class="item col-md-12">
+               <div class="product-miniature item-one first-item d-flex">
+                <div class="thumbnail-container border">
+                 <a href="product-detail.php?id=<?php echo $element['id'] ?>">
+                  <img class="img-fluid" src="img/product/<?php echo $element['product_image'] ?>" alt="img">
+                 </a>
+                </div>
+                <div class="product-description">
+                 <div class="product-groups">
+                  <div class="product-title">
+                   <a href="product-detail.php?id=<?php echo $element['id'] ?>"><?= $element['product_name'] ?></a>
                   </div>
-                 </div>
-                 <div class="product-group-price">
-                  <div class="product-price-and-shipping">
-                   <span class="price">£28.08</span>
+
+                  <div class="product-group-price">
+                   <div class="product-price-and-shipping">
+                    <span >Sale : <?= $element['discount'] ?>%</span>
+                   </div>
+                  </div>
+                  <div class="product-group-price">
+                   <div class="product-price-and-shipping">
+                    <span class="price"><?= $element['product_price'] ?>Jd</span>
+                   </div>
                   </div>
                  </div>
                 </div>
                </div>
               </div>
-             </div>
-             <div class="item col-md-12">
-              <div class="product-miniature item-one first-item d-flex">
-               <div class="thumbnail-container border">
-                <a href="product-detail.html">
-                 <img class="img-fluid image-cover" src="img/product/2.jpg" alt="img">
-                 <img class="img-fluid image-secondary" src="img/product/11.jpg" alt="img">
-                </a>
-               </div>
-               <div class="product-description">
-                <div class="product-groups">
-                 <div class="product-title">
-                  <a href="product-detail.html">Nulla et justo augue</a>
-                 </div>
-                 <div class="rating">
-                  <div class="star-content">
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
-                  </div>
-                 </div>
-                 <div class="product-group-price">
-                  <div class="product-price-and-shipping">
-                   <span class="price">£31.08</span>
-                  </div>
-                 </div>
-                </div>
-               </div>
-              </div>
-             </div>
-             <div class="item col-md-12">
-              <div class="product-miniature item-one first-item d-flex">
-               <div class="thumbnail-container border">
-                <a href="product-detail.html">
-                 <img class="img-fluid image-cover" src="img/product/3.jpg" alt="img">
-                 <img class="img-fluid image-secondary" src="img/product/14.jpg" alt="img">
-                </a>
-               </div>
-               <div class="product-description">
-                <div class="product-groups">
-                 <div class="product-title">
-                  <a href="product-detail.html">Nulla et justo augue</a>
-                 </div>
-                 <div class="rating">
-                  <div class="star-content">
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
-                   <div class="star"></div>
-                  </div>
-                 </div>
-                 <div class="product-group-price">
-                  <div class="product-price-and-shipping">
-                   <span class="price">£20.08</span>
-                  </div>
-                 </div>
-                </div>
-               </div>
-              </div>
-             </div>
+             <?php } ?>
             </div>
            </div>
 
